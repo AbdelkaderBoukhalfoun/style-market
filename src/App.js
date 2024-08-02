90e1928db5f2b0246c0b84bc7c9279b0eb7e9f58
AbdelkaderBoukhalfoun <abdelkaderboukhalfoun@gmail.com>
2024-08-02 03:25:21 +0100

    added router to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React from 'react';
-
-import HomePage from './pages/homepage/homepage.component';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'; // Import Link and useNavigate
 
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    );
-  }
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <button onClick={() => navigate('/topics')}>Topics</button>
+      <h1>Home Page</h1>
+      <Link to="/topics">Go to Topics</Link>
+    </div>
+  );
+};
+
+const TopicsList = () => (
+  <div>
+    <h1>Topics List</h1>
+  </div>
+);
+
+const TopicDetail = () => (
+  <div>
+    <h1>Topic Detail</h1>
+  </div>
+);
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/topics" element={<TopicsList />} />
+        <Route path="/topics/:topicId" element={<TopicDetail />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
