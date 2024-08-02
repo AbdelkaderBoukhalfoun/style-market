daf908623b858022550f91a99a3d15410c978ce3
AbdelkaderBoukhalfoun <abdelkaderboukhalfoun@gmail.com>
2024-08-02 17:10:13 +0100

    added preview collection files and data file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom'; // Import Link and useNavigate
-
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+import './App.css';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/hats" element={<HatsPage />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
     </div>
   );
