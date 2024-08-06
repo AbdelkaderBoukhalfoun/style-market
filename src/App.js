c9e4fb46078b6032bcdf81ed4286e17dd0d2013a
AbdelkaderBoukhalfoun <abdelkaderboukhalfoun@gmail.com>
2024-08-06 22:56:09 +0100

    adding signin page without stlyes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+
 import { Routes, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up.component/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import './App.css';
 
@@ -12,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
+        <Route path="/SignIn" element={<SignInAndSignUpPage />} />
       </Routes>
     </div>
   );
