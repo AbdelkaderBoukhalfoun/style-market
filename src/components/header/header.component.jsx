a54f9f84fc2fd96a00abfd40f31751df35daf40a
AbdelkaderBoukhalfoun <abdelkaderboukhalfoun@gmail.com>
2024-09-26 23:42:10 +0100

    added cartIcon and cartDropDown conponent to application, also implemented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,12 +4,14 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart/cart-dropdown/cart-dropdown.component";
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
         <div className="header">
             <Link className="logo-container" to="/">
                 <Logo className="logo" />
@@ -28,12 +30,17 @@ const Header = ({ currentUser }) => (
                         <Link className="option" to='/signin'>SIGN IN</Link>
                     )
                 }
+                <CartIcon />
             </div>
+            {
+                hidden ? null : <CartDropdown />
+            }
         </div>
 );
 
-const mapStateProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 })
 
 export default connect (mapStateProps)(Header);
